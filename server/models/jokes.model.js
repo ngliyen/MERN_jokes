35bf2bb00c4c322bd922504dc7bd072ceecc8ab0
Li Yen Ng <ngliyen1510@gmail.com>
2021-09-10 12:13:56 -0700

    added validations for jokes model

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const JokeSchema = new mongoose.Schema({
-  setup: {type: String},
-  punchline: {type:String}
+  setup: {
+    type: String,
+    minlength: [10, "Setup must be at least 10 characters long"]
+  },
+  punchline: {
+    type:String,
+    minlength: [3, "Punchline must be at least 3 characters long"]
+  }
 }, {timestamps:true});
 
 const Joke = mongoose.model("joke", JokeSchema);
